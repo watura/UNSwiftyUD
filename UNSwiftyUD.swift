6eb097de886eec9783443a38c95b356390f044c8
Wataru NISHIMOTO <watura@g91.org>
2016-07-09 12:40:25 +0900

    compatible with iOS 8

--- UNSwiftyUD.swift
+++ UNSwiftyUD.swift
@@ -13,11 +13,19 @@ class UDBase: NSObject {
         super.init()
         Mirror(reflecting: self).children.forEach { child in
             if let label = child.label {
-                if let object = ud.object(forKey: path(label: label)) {
-                    setValue(object, forKey:  actualPath(label: label))
-                }
-                addObserver(self, forKeyPath: actualPath(label: label), options: [.new, .old], context: nil)
-                ud.addObserver(self, forKeyPath: path(label: label), options: [.new, .old], context: nil)
+                #if swift(>=3.0)
+                    if let object = ud.object(forKey: path(label: label)) {
+                        setValue(object, forKey:  actualPath(label: label))
+                    }
+                    addObserver(self, forKeyPath: actualPath(label: label), options: [.new, .old], context: nil)
+                    ud.addObserver(self, forKeyPath: path(label: label), options: [.new, .old], context: nil)
+                #else
+                    if let object = ud.objectForKey(path(label)) {
+                        setValue(object, forKey:  actualPath(label))
+                    }
+                    addObserver(self, forKeyPath: actualPath(label), options: [.New, .Old], context: nil)
+                    ud.addObserver(self, forKeyPath: path(label), options: [.New, .Old], context: nil)
+                #endif
             }
         }
     }
@@ -25,34 +33,65 @@ class UDBase: NSObject {
     deinit {
         Mirror(reflecting: self).children.forEach { child in
             if let label = child.label {
-                removeObserver(self, forKeyPath: actualPath(label: label))
-                ud.removeObserver(self, forKeyPath: path(label: label))
+                #if swift(>=3.0)
+                    removeObserver(self, forKeyPath: actualPath(label: label))
+                    ud.removeObserver(self, forKeyPath: path(label: label))
+                #else
+                    removeObserver(self, forKeyPath: actualPath(label))
+                    ud.removeObserver(self, forKeyPath: path(label))
+                #endif
             }
         }
     }
-
-    override func observeValue(forKeyPath keyPath: String?, of object: AnyObject?, change: [NSKeyValueChangeKey : AnyObject]?, context: UnsafeMutablePointer<Void>?) {
-        if let keyPath = keyPath,
-            change = change,
-            newValue = change["new" as NSKeyValueChangeKey],
-            oldValue = change["old" as NSKeyValueChangeKey]  where !newValue.isEqual(oldValue) {
-            setValue(newValue, forKey: actualPath(label: keyPath))
-            ud.set(newValue, forKey: path(label: keyPath) )
-            ud.synchronize()
+    #if swift(>=3.0)
+        override func observeValue(forKeyPath keyPath: String?, of object: AnyObject?, change: [NSKeyValueChangeKey : AnyObject]?, context: UnsafeMutablePointer<Void>?) {
+            if let keyPath = keyPath,
+                change = change,
+                newValue = change["new" as NSKeyValueChangeKey],
+                oldValue = change["old" as NSKeyValueChangeKey]  where !newValue.isEqual(oldValue) {
+                setValue(newValue, forKey: actualPath(label: keyPath))
+                ud.set(newValue, forKey: path(label: keyPath) )
+                ud.synchronize()
+            }
         }
-    }
+    #else
+        override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+            if let keyPath = keyPath,
+                change = change,
+                newValue = change["new"],
+                oldValue = change["old"]  where !newValue.isEqual(oldValue) {
+                setValue(newValue, forKey: actualPath(keyPath))
+                ud.setObject(newValue, forKey: path(keyPath))
+                ud.synchronize()
+            }
+        }
+    #endif
 }
 
 private extension UDBase {
-    var ud: UserDefaults {
-        return UserDefaults.standard
-    }
+    #if swift(>=3.0)
+        var ud: UserDefaults {
+            return UserDefaults.standard
+        }
 
-    func path(label: String) -> String {
-        return "\(self.dynamicType)-" + actualPath(label: label)
-    }
+        func path(label: String) -> String {
+            return "\(self.dynamicType)-" + actualPath(label: label)
+        }
 
-    func actualPath(label: String) -> String {
-        return label.replacingOccurrences(of: "\(self.dynamicType)-", with: "")
-    }
+        func actualPath(label: String) -> String {
+            return label.replacingOccurrences(of: "\(self.dynamicType)-", with: "")
+        }
+    #else
+        var ud: NSUserDefaults {
+            return NSUserDefaults.standardUserDefaults()
+        }
+
+        func path(label: String) -> String {
+            return "\(self.dynamicType)-" + actualPath(label)
+        }
+
+        func actualPath(label: String) -> String {
+            return label.stringByReplacingOccurrencesOfString("\(self.dynamicType)-", withString: "")
+        }
+    #endif
 }
--- UNSwiftyUDTests/UNSwiftyUDTests.swift
+++ UNSwiftyUDTests/UNSwiftyUDTests.swift
@@ -21,7 +21,7 @@ class Fuga: UDBase {
 }
 
 class SwiftyUDTests: XCTestCase {
-
+    #if swift(>=3.0)
     override func setUp() {
         super.setUp()
         UserDefaults.standard.dictionaryRepresentation().keys.forEach { key in
@@ -58,6 +58,51 @@ class SwiftyUDTests: XCTestCase {
         XCTAssertEqual(hoge.string, UserDefaults.standard.string(forKey: "Hoge-string"))
     }
 
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+        // Put the code you want to measure the time of here.
+        }
+    }
+
+    #else
+    override func setUp() {
+        super.setUp()
+        NSUserDefaults.standardUserDefaults().dictionaryRepresentation().keys.forEach { key in
+            NSUserDefaults.standardUserDefaults().removeObjectForKey(key)
+        }
+        NSUserDefaults.standardUserDefaults().synchronize()
+        NSUserDefaults.standardUserDefaults().setObject("a", forKey: "Hoge-string")
+        NSUserDefaults.standardUserDefaults().setObject(1, forKey: "Hoge-int")
+        NSUserDefaults.standardUserDefaults().setObject("z", forKey: "Fuga-string")
+        NSUserDefaults.standardUserDefaults().setObject(99, forKey: "Fuga-int")
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testInt() {
+        let hoge = Hoge()
+        XCTAssertEqual(hoge.int, NSUserDefaults.standardUserDefaults().integerForKey("Hoge-int"))
+        hoge.int = 2
+        XCTAssertEqual(hoge.int, NSUserDefaults.standardUserDefaults().integerForKey("Hoge-int"))
+        NSUserDefaults.standardUserDefaults().setObject(3, forKey: "Hoge-int")
+        XCTAssertEqual(hoge.int, NSUserDefaults.standardUserDefaults().integerForKey("Hoge-int"))
+    }
+
+    func testString() {
+        let hoge = Hoge()
+        XCTAssertEqual(hoge.string, NSUserDefaults.standardUserDefaults().stringForKey("Hoge-string"))
+        hoge.string = "b"
+        XCTAssertEqual(hoge.string, NSUserDefaults.standardUserDefaults().stringForKey("Hoge-string"))
+        NSUserDefaults.standardUserDefaults().setObject("c", forKey: "Hoge-string")
+        XCTAssertEqual(hoge.string, NSUserDefaults.standardUserDefaults().stringForKey("Hoge-string"))
+    }
+    #endif
     func testSeparate() {
         let hoge = Hoge()
         let fuga = Fuga()
@@ -65,10 +110,4 @@ class SwiftyUDTests: XCTestCase {
         XCTAssertNotEqual(hoge.int, fuga.int)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }    
 }
